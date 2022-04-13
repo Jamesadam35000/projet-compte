3a04b53b9c71d8dbdc2459b34e0338fa508f3bfb
james <adamjames35480@gmail.com>
2022-04-13 10:50:19 +0200

    maj appli 3

--- test.py
+++ test.py
@@ -1,15 +1,36 @@
 from Compte import Compte
+from Compte import CompteCourant
+from Compte import CompteEpargne
 
-compte = Compte(8523, 14515161, "James")
-compte.afficherSolde()
+# test de mon affichage du solde de ma classe compte
 
-compte.retrait(256)
-compte.afficherSolde()
-compte.retrait(-256)
-compte.afficherSolde()
+# compte = Compte(60, 14515161, "James")
+# # compte.afficherSolde()
+# #
+# # # Test de la méthode retrait de ma classe compte
+# #
+# compte.retrait(-52)
+# compte.afficherSolde()
+#
+#
+# # Test de la methode versement de ma classe compte
+#
+# compte.versement(256)
+# compte.afficherSolde()
+# compte.versement(-256)
+# compte.afficherSolde()
+#
+# Test de ma classe CompteCourant
 
-compte.versement(256)
-compte.afficherSolde()
-compte.versement(-256)
-compte.afficherSolde()
+CompteCourant = CompteCourant(100,0.1,100,14514521,"James")
+CompteCourant.retrait(110)
+CompteCourant.afficherSolde()
+
+# # Test de ma classe CompteEpargne
+#
+CompteEpargne = CompteEpargne(0.1,1000,14515161,"James")
+CompteEpargne.afficherSolde()
+CompteEpargne.versement(100)
+CompteEpargne.appliquerInteret()
+CompteEpargne.afficherSolde()
 
